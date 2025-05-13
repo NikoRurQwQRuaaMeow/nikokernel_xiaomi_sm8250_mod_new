ca561f1ca1aa0cdf17e5f1b39d00d26c45568234
stic-server-open <1138705738@qq.com>
2025-05-13 10:48:25 +0800

    config: kernelsu: Overlay kernelsu.config to enable CONFIG_KSU=y
    Change-Id: I19f285742f29e4a301788aba8656b5f1f8023f1e

--- release.sh
+++ release.sh
@@ -14,7 +14,7 @@ make O=out ARCH=arm64 CC=clang \
 CLANG_TRIPLE=aarch64-linux-gnu- \
 CROSS_COMPILE=aarch64-linux-gnu- \
 ${product_name}_user_defconfig \
-xiaomi.config
+xiaomi.config kernelsu.config
 
 make -j24 O=out ARCH=arm64 CC=clang \
 CLANG_TRIPLE=aarch64-linux-gnu- \
